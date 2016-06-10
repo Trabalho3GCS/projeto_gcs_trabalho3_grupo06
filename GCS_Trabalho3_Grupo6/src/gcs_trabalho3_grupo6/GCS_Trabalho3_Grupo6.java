5d4de31c215ab124013e37c894d8a37e72c6dfb8
josimarMorais <josimar_morais@outlook.com>
2016-06-10 00:33:49 -0300

    Criação do projeto referente ao trabalho 3 de GCS

---
+++ GCS_Trabalho3_Grupo6/src/gcs_trabalho3_grupo6/GCS_Trabalho3_Grupo6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gcs_trabalho3_grupo6;
+
+/**
+ *
+ * @author Josimar
+ */
+public class GCS_Trabalho3_Grupo6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
